34ba14af5aa5228826b5c68f06d3435eec4825bc
iSiNrus <magnit_322@rambler.ru>
2022-01-26 17:58:06 +0400

    Мало что сделано. не только лишь все, мало что

--- app/src/main/java/ru/barsik/simbirtaskmanager/MainActivity.kt
+++ app/src/main/java/ru/barsik/simbirtaskmanager/MainActivity.kt
@@ -2,10 +2,27 @@ package ru.barsik.simbirtaskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CalendarView
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.barsik.simbirtaskmanager.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    private val binder by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binder.recyclerview.layoutManager = LinearLayoutManager(this)
+
+
+        binder.calendarView.setOnDateChangeListener { calendarView: CalendarView, i: Int, i2: Int, i3: Int ->
+            var date = Date(calendarView.date)
+            Log.d(TAG, "onCreate: $date")
+        }
     }
 }
